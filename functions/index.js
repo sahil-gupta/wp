0baaac513974e0bb32e1880944f093edcedcb864
Sahil Gupta <sahil@microchange.io>
2018-06-03 15:18:07 -0700

    new function

--- functions/index.js
+++ functions/index.js
@@ -3,3 +3,7 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello world---");
 });
+
+exports.helloWorldStaging = functions.https.onRequest((request, response) => {
+ response.send("Hello world staging---");
+});
